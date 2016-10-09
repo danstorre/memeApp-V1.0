9c69e83eb840253120e4e5af656c3a34c84b2b58
Daniel Torres <dtorres@4geeks.com.ve>
2016-10-09 15:50:37 -0400

    adding some files

---
+++ MemeMe 1.0/SentMemesCollectionViewController.swift
@@ -0,0 +1,85 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Daniel Torres on 10/5/16.
+//  Copyright © 2016 Danieltorres. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewController: UICollectionViewController {
+
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    var memes: [Meme] {
+        return (UIApplication.shared.delegate as! AppDelegate).memes
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let space: CGFloat = 3
+        let width = (view.frame.size.width - (2 * space)) / 3.0
+        
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: width, height: 95)
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView?.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addAMeme(_ sender: AnyObject) {
+        let memeEditorVC = self.storyboard!.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeEditorViewController
+        let defaultMeme = Meme()
+        
+        memeEditorVC.aMeme = defaultMeme
+        
+        self.present(memeEditorVC, animated: true, completion: nil)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
+        let meme = self.memes[indexPath.row]
+        
+        // Set image meme
+        cell.cellMemeImage.image = meme.memeImage
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let memeEditorVC = self.storyboard!.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeEditorViewController
+        let selectedMeme = memes[indexPath.row]
+        
+        memeEditorVC.aMeme = selectedMeme
+        
+        self.present(memeEditorVC, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MemeMe 1.0/SentMemesTableVC.swift
@@ -0,0 +1,84 @@
+//
+//  SentMemesTableVC.swift
+//  MemeMe 1.0
+//
+//  Created by Daniel Torres on 10/5/16.
+//  Copyright © 2016 Danieltorres. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesTableVC: UITableViewController {
+
+    var memes: [Meme] {
+        return (UIApplication.shared.delegate as! AppDelegate).memes
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addAMeme(_ sender: AnyObject) {
+        
+        let memeEditorVC = self.storyboard!.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeEditorViewController
+        let defaultMeme = Meme()
+        
+        memeEditorVC.aMeme = defaultMeme
+        
+        self.present(memeEditorVC, animated: true, completion: nil)
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "memeCell", for: indexPath)
+        let meme = self.memes[indexPath.row]
+        
+        // Set image meme
+        cell.textLabel?.text = "\(meme.topText) ... \(meme.bottomText)"
+        cell.imageView?.image = meme.memeImage
+        
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let memeEditorVC = self.storyboard!.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeEditorViewController
+        let selectedMeme = memes[indexPath.row]
+        
+        memeEditorVC.aMeme = selectedMeme
+        
+        self.present(memeEditorVC, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
